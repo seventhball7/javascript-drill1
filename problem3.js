95fa604650fbd643a285538b52a9ef6a07428516
sameer srivastava <sameersrivastava8055@gmail.com>
2021-08-11 10:45:57 +0530

    modified problem3

--- problem3.js
+++ problem3.js
@@ -4,7 +4,15 @@ pb3 = (arr) =>{
         cmodels.push(arr[i].car_model);
         
     }
-    cmodels.sort();
+    for(let i=0;i<cmodels.length;i++){
+        for(j=i+1;j<cmodels.length;j++){
+            if(cmodels[i]>cmodels[j]){
+                let temp=cmodels[i];
+                cmodels[i]=cmodels[j];
+                cmodels[j]=temp;
+            }
+        }
+    }
     return cmodels;
 }
 module.exports=(pb3);
